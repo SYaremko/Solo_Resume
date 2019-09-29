a81c81b2c7f8bf886ecc306ccbf752d04c5379e2
Solomiia Yaremko <19solyaremko@gmail.com>
2019-09-29 22:40:58 +0300

    add dataConfig class

--- src/main/java/ua/com/Solo_Resume/service/NameServiceImp.java
+++ src/main/java/ua/com/Solo_Resume/service/NameServiceImp.java
@@ -1,17 +1,20 @@
 package ua.com.Solo_Resume.service;
 
 import org.apache.commons.lang.WordUtils;
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Service;
 
 @Service
-public class NameServiceImp implements NameServise{
+public class NameServiceImp implements NameServise {
+    private static final Logger logger = Logger.getLogger(NameServiceImp.class);
 
     @Override
     public String convertName(String name) {
-    if(name.contains("-")){
-        String[] parts = name.split("-");
-        return WordUtils.capitalize(parts[0]) + " " + WordUtils.capitalize(parts[1]);
-    }
+        logger.debug("solomiia lala");
+        if (name.contains("-")) {
+            String[] parts = name.split("-");
+            return WordUtils.capitalize(parts[0]) + " " + WordUtils.capitalize(parts[1]);
+        }
         return WordUtils.capitalize(name);
     }
 }
